a785752bc3e1d169f06cd384fd8783dddc7a0e4e
jiabo <843051627@qq.com>
2026-08-26 00:47:06 +0800

    test: acceptance against unfixed code

---
+++ internal/ingest/pipeline.go
@@ -0,0 +1,44 @@
+package ingest
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/jb843051627/fjord-resonance/internal/model"
+)
+
+type Sink interface {
+	Put(context.Context, model.AcousticSample) error
+}
+
+type Pipeline struct {
+	sink   Sink
+	dedupe *Dedupe
+	clock  func() time.Time
+}
+
+func NewPipeline(sink Sink) *Pipeline {
+	return &Pipeline{sink: sink, dedupe: NewDedupe(), clock: time.Now}
+}
+
+func (p *Pipeline) Accept(ctx context.Context, frame Frame) error {
+	if err := ctx.Err(); err != nil {
+		return err
+	}
+	sample, err := DecodeFrame(frame)
+	if err != nil {
+		return err
+	}
+	key := fmt.Sprintf("%s:%d", sample.SensorID, sample.Sequence)
+	if !p.dedupe.Accept(key) {
+		return nil
+	}
+	if err := p.sink.Put(ctx, sample); err != nil {
+		p.dedupe.Forget(key)
+		return fmt.Errorf("pipeline sink: %w", err)
+	}
+	return nil
+}
+
+func (p *Pipeline) Reset() { p.dedupe = NewDedupe() }
